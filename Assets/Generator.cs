51664cdf6bf528197424a6af01aeb71c230b0873
Daniel <daniel@manifestohut.com.br>
2019-09-09 19:39:41 -0300

    adding initial project

---
+++ Assets/Generator.cs
@@ -0,0 +1,62 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+public class Generator : MonoBehaviour {
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.A)) {
+            GenerateJson();
+        }
+    }
+
+    public void GenerateJson() {
+        List<GameObject> list = new List<GameObject>();
+        Transform t = transform;
+        int childCount = t.childCount;
+        print(childCount + " children");
+        for(int i = 0; i < childCount; i++) {
+            list.Add(t.GetChild(i).gameObject);
+        }
+
+        list = list.OrderBy(x => x.transform.position.x).ToList();
+        print("list has " + list.Count + " elements");
+        string s = "{\n";
+
+        for(int i = 0; i < childCount; i++) {
+            GameObject g = list[i];
+            s += "\t";
+            s += $"{{\"name\": \"{g.GetComponent<SpriteRenderer>().sprite.name}\", ";
+            s += $"\"time\": {GetTime(g.transform.position.x)}, ";
+            s += $"\"yScale\": {GetYScale(g.transform.position.y)}}}";
+            if (i == childCount - 1) s += "\n";
+            else s += ",\n";
+        }
+        s += "}";
+        Debug.Log(s);
+    }
+
+    float x0 = 16;
+    float GetTime(float x) {
+        return (x - x0) / MoveCamera.speed; 
+    }
+
+    float GetYScale(float y) {
+        return y / 9f;
+    }
+
+}
+
+[CustomEditor(typeof(Generator))]
+public class GeneratorEditor : Editor {
+    public override void OnInspectorGUI() {
+        base.OnInspectorGUI();
+
+        if(GUILayout.Button("Generate JSON")) {
+            Generator g = (Generator) target;
+            g.GenerateJson();
+        }
+    }
+}
---
+++ Assets/MoveCamera.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour {
+    public bool x1;
+    public bool x2;
+    public bool x4;
+
+    int curr = 1;
+
+
+    public static float speed = 0.04f;
+	
+	// Update is called once per frame
+	void Update () {
+        if (curr == 1) {
+            if (x2) {
+                x1 = false;
+                x4 = false;
+                curr = 2;
+            }
+            else if (x4) {
+                x1 = false;
+                x2 = false;
+                curr = 4;
+            }
+        }
+        else if (curr == 2) {
+            if (x1) {
+                x2 = false;
+                x4 = false;
+                curr = 1;
+            }
+            if (x4) {
+                x1 = false;
+                x2 = false;
+                curr = 4;
+            }
+        }
+        else if (curr == 4) {
+            if (x1) {
+                x2 = false;
+                x4 = false;
+                curr = 1;
+            }
+            if (x2) {
+                x1 = false;
+                x4 = false;
+                curr = 2;
+            }
+        }
+        transform.Translate((float) curr * speed, 0, 0);
+	}
+}
